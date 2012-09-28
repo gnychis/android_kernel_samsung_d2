6adbbc3b2329a8d2ded546e54e2c33d83eedf427
George Nychis <gnychis@gmail.com>
2012-09-27 23:43:10 -0400

    adding the config straight from the device

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 make ARCH=arm clean
-make ARCH=arm cyanogen_d2_awmon_defconfig
+#make ARCH=arm cyanogen_d2_defconfig
+make ARCH=arm cyanogen_d2_from_device_defconfig
 make -j2 ARCH=arm CROSS_COMPILE=arm-eabi-
 ./package.sh
--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ rm -f package_tree/boot.img
 find . -name "*.ko" -exec cp {} package_tree/system/lib/modules \;
 ./mkbootfs initramfs | gzip > ramdisk.gz
 cp arch/arm/boot/zImage .
-./mkbootimg --kernel zImage --ramdisk ramdisk.gz --cmdline "androidboot.hardware=qcom user_debug=31" -o boot.img --base 0x80200000 --pagesize 2048
+./mkbootimg --kernel zImage --ramdisk ramdisk.gz --cmdline "androidboot.hardware=qcom user_debug=31" -o boot.img --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000
 cp boot.img package_tree
 rm zImage
 cd package_tree
