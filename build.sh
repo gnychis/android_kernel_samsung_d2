b0a9a296575000a34a53836a2d9caebe24287f6c
George Nychis <gnychis@gmail.com>
2012-09-30 14:16:41 -0400

    adding in a Makefile for linaro

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-make ARCH=arm clean
-#make ARCH=arm cyanogen_d2_defconfig
-make ARCH=arm cyanogen_d2_from_device_defconfig
-make -j2 ARCH=arm CROSS_COMPILE=arm-eabi-
+export ARCH=arm
+export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
+make clean
+make cyanogen_d2_defconfig
+make -j2 
 ./package.sh
--- package.sh
+++ package.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 rm -f sgs3_cm10_awmon.zip
-rm -f initramfs/lib/modules/*
 rm -f package_tree/system/lib/modules/*
 rm -f package_tree/boot.img
 find . -name "*.ko" -exec cp {} package_tree/system/lib/modules \;
-./mkbootfs initramfs | gzip > ramdisk.gz
+./mkbootfs initramfs-cm10 | gzip > ramdisk.gz
 cp arch/arm/boot/zImage .
 ./mkbootimg --kernel zImage --ramdisk ramdisk.gz --cmdline "androidboot.hardware=qcom user_debug=31" -o boot.img --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000
 cp boot.img package_tree
