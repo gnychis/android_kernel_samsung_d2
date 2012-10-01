805f5b9304e2071303f4dcde932f6b665b7ff8e9
George Nychis <gnychis@gmail.com>
2012-10-01 00:57:59 -0400

    switching over to the awmon config with rt2x00

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 export ARCH=arm
 export CROSS_COMPILE=`readlink -f ../linaro-android-toolchain`/bin/arm-eabi-
 make clean
-make cyanogen_d2_defconfig
+make cyanogen_d2_awmon_defconfig
 make -j2 
 ./package.sh
