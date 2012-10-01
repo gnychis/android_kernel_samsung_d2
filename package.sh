8d737fab7d928b6f4432459e9b1a76426557fc79
George Nychis <gnychis@gmail.com>
2012-10-01 01:20:05 -0400

    adding initramfs

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@ rm -f sgs3_cm10_awmon.zip
 rm -f package_tree/system/lib/modules/*
 rm -f package_tree/boot.img
 find . -name "*.ko" -exec cp {} package_tree/system/lib/modules \;
-./mkbootfs initramfs-cm10 | gzip > ramdisk.gz
+./mkbootfs initramfs | gzip > ramdisk.gz
 cp arch/arm/boot/zImage .
 ./mkbootimg --kernel zImage --ramdisk ramdisk.gz --cmdline "console=null androidboot.hardware=qcom user_debug=31" -o boot.img --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000
 cp boot.img package_tree
