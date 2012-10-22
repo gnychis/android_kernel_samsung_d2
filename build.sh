7d73c43bc9ea2863a0508a150c1b5bca7fe5c900
George Nychis <gnychis@gmail.com>
2012-10-22 14:42:42 -0700

    adding in an external wireless configuration to build the module outside

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ export ARCH=arm
 export CROSS_COMPILE=`readlink -f ../os/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6`/bin/arm-eabi-
 #export CROSS_COMPILE=`readlink -f ../os/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3`/bin/arm-eabi-
 make clean
-make cyanogen_d2_awmon_defconfig
+#make cyanogen_d2_awmon_defconfig
+make cyanogen_d2_awmon_extwireless_defconfig
 make -j2 
 ./package.sh
--- package.sh
+++ package.sh
@@ -3,6 +3,10 @@ rm -f sgs3_cm10_awmon.zip
 rm -f package_tree/system/lib/modules/*
 rm -f package_tree/boot.img
 find . -name "*.ko" -exec cp {} package_tree/system/lib/modules \;
+cd ../compat-wireless-sgs3
+./make.sh
+cd ../sgs3-cm10-kernel
+find ../compat-wireless-sgs3/ -name "*.ko" -exec cp {} package_tree/system/lib/modules/ \;
 ./mkbootfs initramfs | gzip > ramdisk.gz
 cp arch/arm/boot/zImage .
 ./mkbootimg --kernel zImage --ramdisk ramdisk.gz --cmdline "console=null androidboot.hardware=qcom user_debug=31" -o boot.img --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000
